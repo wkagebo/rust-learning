061baf1670011809bc87ab48063052e48fd197ee
wkagebo <kagebo@kth.se>
2024-07-09 00:15:41 +0300

    Add twelve_days_of_christmas exercise

---
+++ twelve_days_of_christmas/src/main.rs
@@ -0,0 +1,52 @@
+const COUNT: [&str; 12] = [
+    "first", 
+    "second", 
+    "third", 
+    "fourth", 
+    "fifth", 
+    "sixth", 
+    "seventh", 
+    "eighth", 
+    "ninth", 
+    "tenth", 
+    "eleventh", 
+    "twelfth"
+]; 
+
+const GIFTS: [&str; 12] = [
+    "a partridge in a pear tree",
+    "Two turtle doves",
+    "Three French hens",
+    "Four calling birds",
+    "Five golden rings",
+    "Six geese a-laying",
+    "Seven swans a-swimming",
+    "Eight maids a-milking",
+    "Nine ladies dancing",
+    "Ten lords a-leaping",
+    "Eleven pipers piping",
+    "Twelve drummers drumming"
+];
+
+fn print_intro(verse: usize) {
+    println!("On the {} day of Christmas", COUNT[verse]);
+    println!("My true love gave to me")
+} 
+
+fn sing() {
+    for verse in 0..12 {
+        print_intro(verse); 
+        for gift in (0..=verse).rev() {
+            if verse != 0 && gift == 0 {
+                println!("And {}", GIFTS[gift]); 
+            } else {
+                println!("{}", GIFTS[gift]);
+            }
+        }
+        println!(""); 
+    }
+}
+
+fn main() {
+    sing()
+}
